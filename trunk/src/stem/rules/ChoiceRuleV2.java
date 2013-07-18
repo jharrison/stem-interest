6ed623ad02ddcd5adb2612623768d4ef929c52e5
joey.f.harrison@gmail.com <joey.f.harrison@gmail.com@46883a90-869f-ece3-d1fb-cc689642acf6>
2013-07-18 16:04:08 +0000

    Added a model accessor to show a histogram of friend counts. Added ChoiceRuleV2 and UnrelatedAdultRuleV2.

---
+++ trunk/src/stem/rules/ChoiceRuleV2.java
@@ -0,0 +1,34 @@
+package stem.rules;
+
+import stem.Student;
+import stem.activities.Activity;
+
+/**
+ * Choice<BR>
+ * <ul>
+ * <li>If the student has a low degree of choice, interest and participation decrease.</li>
+ * <li>If the student has a moderate degree of choice, no change.</li>
+ * <li>If the student has a high degree of choice, interest and participation increases.</li>
+ * </ul>
+ * 
+ * @author Joey Harrison
+ *
+ */
+public class ChoiceRuleV2 extends Rule
+{
+	@Override
+	public void apply(Student s, Activity a) {
+		switch (a.type.degreeOfChoice) {
+		case 0: // Low: decrease interest and participation
+			s.decreaseInterest(a.content, weight);
+			s.decreaseParticipationRate(a.type.id);
+			break;
+		case 1: // Moderate: no change
+			break;
+		case 2: // High: increase interest and participation
+			s.increaseInterest(a.content, weight);
+			s.increaseParticipationRate(a.type.id);
+			break;
+		}
+	}
+}
--- trunk/src/stem/rules/RuleSet.java
+++ trunk/src/stem/rules/RuleSet.java
@@ -11,15 +11,22 @@ public class RuleSet
 	FriendRule friendRule = new FriendRule();
 	ParentRule parentRule = new ParentRule();
 	UnrelatedAdultRule unrelatedAdultRule = new UnrelatedAdultRule();
+	UnrelatedAdultRuleV2 unrelatedAdultRuleV2 = new UnrelatedAdultRuleV2();
 	ChoiceRule choiceRule = new ChoiceRule();
+	ChoiceRuleV2 choiceRuleV2 = new ChoiceRuleV2();
 	MakeFriendRule makeFriendRule = new MakeFriendRule();
 	
 	public RuleSet() {
 		rules.add(friendRule);
 		rules.add(parentRule);
 		rules.add(unrelatedAdultRule);
+		rules.add(unrelatedAdultRuleV2);
 		rules.add(choiceRule);
+		rules.add(choiceRuleV2);
 		rules.add(makeFriendRule);
+		
+		unrelatedAdultRule.isActive = false;
+		choiceRule.isActive = false;
 	}
 	
 	public boolean getFriendRuleEnabled() { return friendRule.isActive; }
@@ -40,13 +47,25 @@ public class RuleSet
 	public void setUnrelatedAdultRuleWeight(double val) { unrelatedAdultRule.weight = val; }
 	public Object domUnrelatedAdultRuleWeight() { return new Interval(0.0, 5.0); }
 	
+	public boolean getUnrelatedAdultRuleV2Enabled() { return unrelatedAdultRuleV2.isActive; }
+	public void setUnrelatedAdultRuleV2Enabled(boolean val) { unrelatedAdultRuleV2.isActive = val; }	
+	public double getUnrelatedAdultRuleV2Weight() { return unrelatedAdultRuleV2.weight; }
+	public void setUnrelatedAdultRuleV2Weight(double val) { unrelatedAdultRuleV2.weight = val; }
+	public Object domUnrelatedAdultRuleV2Weight() { return new Interval(0.0, 5.0); }
+	
 	public boolean getChoiceRuleEnabled() { return choiceRule.isActive; }
 	public void setChoiceRuleEnabled(boolean val) { choiceRule.isActive = val; }	
 	public double getChoiceRuleWeight() { return choiceRule.weight; }
 	public void setChoiceRuleWeight(double val) { choiceRule.weight = val; }
 	public Object domChoiceRuleWeight() { return new Interval(0.0, 5.0); }
 	
-
+	public boolean getChoiceRuleV2Enabled() { return choiceRuleV2.isActive; }
+	public void setChoiceRuleV2Enabled(boolean val) { choiceRuleV2.isActive = val; }	
+	public double getChoiceRuleV2Weight() { return choiceRuleV2.weight; }
+	public void setChoiceRuleV2Weight(double val) { choiceRuleV2.weight = val; }
+	public Object domChoiceRuleV2Weight() { return new Interval(0.0, 5.0); }
+	
 	public boolean getMakeFriendRuleEnabled() { return makeFriendRule.isActive; }
+	public void setMakeFriendRuleEnabled(boolean val) { makeFriendRule.isActive = val; }	
 
 }
