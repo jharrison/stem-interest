c03444fd57148bae442b905f6abe27fb96407576
matthew.hendrey@gmail.com <matthew.hendrey@gmail.com@46883a90-869f-ece3-d1fb-cc689642acf6>
2015-01-19 15:48:59 +0000

    Now using the 6th grade data for initializing the model.  There are now 144 students in the data.  Some of the activities were broken out into multiple subcategories, while others were wholly changed.

--- trunk/scripts/createInitialStudentInput.R
+++ trunk/scripts/createInitialStudentInput.R
@@ -18,7 +18,7 @@ surveyAll <- read.csv("original/survey_may_2014_abm_datavaluesRemoveCtrlM.dat", 
 # To grab only the 5th grade surveys, ID == 1 (174 of these)
 # If I wanted the 6th grade surveys, ID_2 == 3 (144 of these)
 # If I wanted the 7th grade surveys, ID_3 == 4 (156 of these)
-survey5 <- surveyAll[surveyAll$ID == 1 & !is.na(surveyAll$ID),];
+survey6 <- surveyAll[surveyAll$ID_2 == 3 & !is.na(surveyAll$ID_2),];
 
 # Get only select columns to be pulled in by ABM
 #   Survey_number, Sex, Elementary School Code, Science Teacher Code
@@ -29,36 +29,46 @@ survey5 <- surveyAll[surveyAll$ID == 1 & !is.na(surveyAll$ID),];
 
 # This allows me to reference the columns directly which helps with A's column names
 # I had to change a couple of misspellings by hand.
-attach(survey5);
-A <- data.frame(Survey_number, Sex, School_code, Teacher_code,
-                Library_me, Museum_me, Scouts_me, National_Parks_me,
-                Afterschool_me, Talk_me, Summer_camp_me,
-                Hike_outdoors_me, Garden_me, Experiments_me, Read_me,
-                Internet_me, Computer_me, TV_me, Build_me,
-                Library_parent,       Library_sibling,       Library_friends,       Library_no_one,
-                Museum_parent,        Museum_siblings,        Museum_friends,        Museum_no_one,
-                Scouts_parents,        Scouts_siblings,        Scouts_friends,        Scouts_no_one,
-                National_park_parents, National_park_siblings, National_park_friends, National_park_no_one,
-                Afterschool_parents,   Afterschool_siblings,   Afterschool_friends,   Afterschool_no_one,
-                Talk_parents,          Talk_siblings,          Talk_friends,          Talk_no_one,
-                Summer_camps_parents,  Summer_camps_siblings,  Summer_camps_friends,  Summer_camps_no_one,
-                Hike_outdoors_parents, Hike_outdoors_siblings, Hike_outdoors_friends, Hike_outdoor_no_one,
-                Garden_parents,        Garden_siblings,        Garden_friends,        Garden_no_one,
-                Experiment_parents,    Experiment_siblings,    Experiment_friends,    Experiment_no_one,
-                Read_parents,          Read_siblings,          Read_friends,          Read_no_one,
-                Internet_parent,      Internet_siblings,      Internet_friends,      Internet_no_one,
-                Computer_parents,      Computer_siblings,      Computer_friends,      Computer_no_one,
-                TV_parents,            TV_siblings,            TV_friends,            TV_no_one,
-                Build_parents,         Build_sibings,          Build_friends,         Build_no_one,
-                Stars_planets, mix_materials, Weather, Human_body,
-                Traits, Dinosaurs, Fish_hunt, Earthquakes,
-                Instruments, Diseases, Dangerous_plants_animals,
-                Planets_space, Buildings_bridges, Eat_exercise,
-                Engines, Rocks_minerals, Computers_cell_phones,
-                Puzzels_math, Food_flowers, Maps, Invent,
-                Community_green, Pets,
-                Earth_space_5, Human_bio_5, Tech_eng_5);
-detach(survey5);
+# The Survey_number and Survey_number_2 are the same
+# The list of activities is slightly different between 6th grade survey and 5th grade
+# The Encouragement section also added a "Do Not Do" box.
+attach(survey6);
+A <- data.frame(Survey_number, Sex, School_code_2, Science_teacher_2,
+                Library_me_2, OMSI_me_2, Zoo_me_2, Child_museum_me_2,
+                Scouts_me_2, Program_me_2, National_parks_me_2,
+                Team_sport_me_2, Own_sport_me_2, Summer_camp_me_2,
+                Hike_outdoors_me_2, Garden_me_2, Experiments_me_2, 
+                Read_me_2, Internet_me_2, Computer_me_2, 
+                Communicate_me_2, TV_me_2, Build_me_2, Pets_me_2,
+                Library_parents_2,        Library_sibling_2,        Library_friends_2,        Library_no_one,          Library_do_not_2,
+                OMSI_parents_2,           OMSI_sibling_2,           OMSI_friends_2,           OMSI_no_one_2,           OMSI_do_not_2,
+                Zoo_parents_2,            Zoo_sibling_2,            Zoo_friend_2,             Zoo_no_one_2,            Zoo_do_not_2,
+                Childrens_parent_2,       Childrens_sibling_2,      Childrens_friend_2,       Childrens_no_one_2,      Childrens_do_not_2,
+                Scouts_parents_2,         Scouts_sibling_2,         Scouts_friends_2,         Scouts_no_one_2,         Scouts_do_not_2,
+                Other_after_parents_2,    Other_after_sibling_2,    Other_after_friend_2,     Other_after_no_one_2,    Other_after_do_not_2,
+                National_park_parents_2,  National_park_sibling_2,  National_park_friends_2,  National_park_no_one_2,  National_park_do_not_2,
+                Team_sport_parent_2,      Team_sport_sibling_2,	    Team_sport_friend_2,	    Team_sport_no_one_2,	   Team_sport_do_not_2,
+                Sport_parent_2,           Sport_sibling_2,	        Sport_friend_2,	          Sport_no_one_2,	         Sport_do_not_2,
+                Summer_camps_parent_2,    Summer_camps_sibling_2,	  Summer_camps_friends_2,	  Summer_camps_no_one_2,	 Summer_camps_do_not_2,
+                Hike_outdoors_parents_2,  Hike_outdoors_siblings_2,	Hike_outdoors_friends_2,	Hike_outdoors_no_one_2,	 Hike_outdoors_do_not_2,
+                Garden_parents_2,         Garden_siblings_2,	      Garden_friends_2,	        Garden_no_one_2,	       Garden_do_not_2,
+                Experiment_parents_2,     Experiment_siblings_2,	  Experiment_friends_2,	    Experiment_no_one_2,	   Experiment_do_not_2,
+                Read_parents_2,           Read_siblings_2,	        Read_friends_2,	          Read_no_one_2,	         Read_do_not_2,
+                Internet_parent_2,        Internet_siblings_2,	    Internet_friends_2,	      Internet_no_one_2,	     Internet_do_not_2,
+                Computer_games_parent_2,  Computer_games_sibling_2,	Computer_games_friend_2,	Computer_games_no_one_2, Computer_games_do_not_2,
+                Communicate_parent_2,     Communicate_sibling_2,	  Communicate_friend_2,	    Communicate_no_one_2,	   Communicate_do_not_2,
+                TV_parents_2,             TV_siblings_2,	          TV_friends_2,	            TV_no_one_2,	           TV_do_not_2,
+                Build_parents_2,          Build_siblings_2,	        Build_friends_2,	        Build_no_one_2,	         Build_do_not_2,
+                Pets_parent_2,            Pets_sibling_2,	          Pets_friend_2,	          Pets_no_one_2,	         Pets_do_not_2,
+                Stars_planets_2,  Mix_materials_2,	Weather_2,	Human_body_2,	
+                Traits_2,	Sudoku_2,	Fish_hunt_2,	Earthquakes_2,	
+                Measure_2,	Diseases_2,	Minecraft_2,	
+                Planets_space_2,	Buildings_bridges_2,	Eat_exercise_2,	
+                Engines_2,	Rocks_minerals_2,	Computers_cell_phones_2,	
+                Food_flowers_2,	Maps_2,	Invent_2,	Community_green_2,	
+                Shapes_2,	Puzzles_2,
+                Earth_space_6, Human_bio_6, Tech_eng_6);
+detach(survey6);
 
 # Normalize and rescale index values between 0 (No Interest) & 1 (Very Interested)
 # Initially they scale from 1 to 5 with some NAs in there
@@ -67,7 +77,7 @@ A[,(ncol(A)-2):ncol(A)] <- (A[,(ncol(A)-2):ncol(A)] - 1 ) / 4;
 
 ####### DATA CLEANING #########
 # Quick scan to see that all activity values are within valid range of 1-5.
-for (i in 5:19)
+for (i in 5:24)
 {
   xmin = min(A[,i], na.rm=TRUE);
   xmax = max(A[,i], na.rm=TRUE);
@@ -85,13 +95,14 @@ for (i in 5:19)
     print(c(indexValue,xmax));
   }
 }
-# This showed that Museum_me at index 40 had a value of 54.
-# Setting it to a value of 4
-A$Museum_me[40] = 4; # Original was 54
+
+# This showed that Scouts_me_2 at index 106 had a value of 0.
+# Setting it to a value of 1
+A$Scouts_me_2[106] = 1; # Original was 0
 
 # Quick scan to see that all encouragement is either 0 or 1
 # Let's set any NA as a 0.
-for (i in 20:79)
+for (i in 25:124)
 {
   xmin = min(A[,i], na.rm=TRUE);
   xmax = max(A[,i], na.rm=TRUE);
@@ -112,14 +123,9 @@ for (i in 20:79)
   A[badRecord,i] = 0;
 }
 
-# This gave some values that exceeeded the maximum of 1
-A$Library_parent[25] = 1;       # Original was 2
-A$Museum_parent[93] = 1;        # Original was 3
-A$Scouts_parents[93] = 1;       # Original was 2
-A$Afterschool_parents[140] = 1; # Original was 2
 
 # Quick scan to see that all Interests are in valid scan
-for (i in 80:102)
+for (i in 125:147)
 {
   xmin = min(A[,i], na.rm=TRUE);
   xmax = max(A[,i], na.rm=TRUE);
@@ -139,9 +145,11 @@ for (i in 80:102)
   print(c(names(A)[i],'has NA at index values'));
   print(indexNA);
 }
+# Index = 86 seems to have a lot of NA for the Interests section
+
 
 # Scan the interest vector
-for (i in 103:105)
+for (i in 148:150)
 {
   xmin = min(A[,i],na.rm=TRUE);
   xmax = max(A[,i],na.rm=TRUE);
@@ -162,17 +170,21 @@ for (i in 103:105)
   print(c(names(A)[i],'has NA at index values'));
   print(indexNA);
 }
+# Index = 86 has NA values for all three indices.
+
 ####### END DATA CLEANING #########
 
-# Keep only records that have no NA's in columns 5:19 (Stuff I Do) & 103:105 (Interest Vector).
+# Keep only records that have no NA's in columns 5:24 (Stuff I Do) & 148:150 (Interest Vector).
 attach(A);
-JJ <- !is.na(Library_me) & !is.na(Museum_me) & !is.na(Scouts_me) & !is.na(National_Parks_me) & !is.na(Afterschool_me) &
-  !is.na(Talk_me) & !is.na(Summer_camp_me) & !is.na(Hike_outdoors_me) & !is.na(Garden_me) & !is.na(Experiments_me) &
-  !is.na(Read_me) & !is.na(Internet_me) & !is.na(Computer_me) & !is.na(TV_me) & !is.na(Build_me) &
-  !is.na(Earth_space_5) & !is.na(Human_bio_5) & !is.na(Tech_eng_5);
+JJ <- !is.na(Library_me_2) & !is.na(OMSI_me_2) & !is.na(Zoo_me_2) & !is.na(Child_museum_me_2) & !is.na(Scouts_me_2) & 
+  !is.na(Program_me_2) & !is.na(National_parks_me_2) & !is.na(Team_sport_me_2) & !is.na(Own_sport_me_2) &
+  !is.na(Summer_camp_me_2) & !is.na(Hike_outdoors_me_2) & !is.na(Garden_me_2) & !is.na(Experiments_me_2) &
+  !is.na(Read_me_2) & !is.na(Internet_me_2) & !is.na(Computer_me_2) & !is.na(Communicate_me_2) & !is.na(TV_me_2) & 
+  !is.na(Build_me_2) & !is.na(Pets_me_2) &
+  !is.na(Earth_space_6) & !is.na(Human_bio_6) & !is.na(Tech_eng_6);
 detach(A);
 
-# This leaves me with 129 records out of 174
+# This leaves me with 99 records out of 144
 B <- A[JJ,];
 
 # Now encode the Encouragement section
@@ -182,51 +194,49 @@ B <- A[JJ,];
 #  friends = 1 (not selected) or 2 (selected)
 #  no_one = 1 (not selected) or 2 (selected)
 N = dim(B)[1];
-Encourage = data.frame(Library_encourage=rep(0,N), Museum_encourage=rep(0,N), Scouts_encourage=rep(0,N), 
-                       National_park_encourage=rep(0,N), Afterschool_encourage=rep(0,N), 
-                       Talk_encourage=rep(0,N), Summer_camps_encourage=rep(0,N), 
+Encourage = data.frame(Library_encourage=rep(0,N), OMSI_encourage=rep(0,N), Zoo_encourage=rep(0,N), Childrens_encourage=rep(0,N), 
+                       Scouts_encourage=rep(0,N), Other_after_encourage=rep(0,N), National_park_encourage=rep(0,N), 
+                       Team_sport_encourage=rep(0,N), Sport_encourage=rep(0,N), Summer_camps_encourage=rep(0,N), 
                        Hike_outdoors_encourage=rep(0,N), Garden_encourage=rep(0,N), Experiment_encourage=rep(0,N),
-                       Read_encourage=rep(0,N), Internet_encourage=rep(0,N), Computer_encourage=rep(0,N), 
-                       TV_encourage=rep(0,N), Build_encourage=rep(0,N));
+                       Read_encourage=rep(0,N), Internet_encourage=rep(0,N), Computer_games_encourage=rep(0,N), 
+                       Communicate_encourage=rep(0,N), TV_encourage=rep(0,N), Build_encourage=rep(0,N), Pets_encourage=rep(0,N));
 for (j in 1:dim(Encourage)[2])
 {
-  i = (j-1)*4 + 20;  # To get the column in B that I want
+  i = (j-1)*5 + 25;  # To get the column in B that I want. Each encouragement has 5 elements (parents, sibling, friend, no_one, do_not)
   Encourage[,j] = (B[,i]+1)*1000 + (B[,i+1]+1)*100 + (B[,i+2]+1)*10 + (B[,i+3]+1);  
 }
 
 # Now to combine the B dataframe (minus all the encouragement stuff) with Encourage
 attach(B); 
 attach(Encourage);
-BB <- data.frame(Survey_number, Sex, School_code, Teacher_code,
-                 Library_me, Museum_me, Scouts_me, National_Parks_me,
-                 Afterschool_me, Talk_me, Summer_camp_me,
-                 Hike_outdoors_me, Garden_me, Experiments_me, Read_me,
-                 Internet_me, Computer_me, TV_me, Build_me,
-                 Library_encourage, Museum_encourage, Scouts_encourage, 
-                 National_park_encourage, Afterschool_encourage, 
-                 Talk_encourage, Summer_camps_encourage, 
-                 Hike_outdoors_encourage, Garden_encourage, Experiment_encourage,
-                 Read_encourage, Internet_encourage, Computer_encourage, 
-                 TV_encourage, Build_encourage,
-                 Stars_planets, mix_materials, Weather, Human_body,
-                 Traits, Dinosaurs, Fish_hunt, Earthquakes,
-                 Instruments, Diseases, Dangerous_plants_animals,
-                 Planets_space, Buildings_bridges, Eat_exercise,
-                 Engines, Rocks_minerals, Computers_cell_phones,
-                 Puzzels_math, Food_flowers, Maps, Invent,
-                 Community_green, Pets,
-                 Earth_space_5, Human_bio_5, Tech_eng_5);
+BB <- data.frame(Survey_number, Sex, School_code_2, Science_teacher_2,
+                 Library_me_2, OMSI_me_2, Zoo_me_2, Child_museum_me_2, Scouts_me_2,
+                 Program_me_2, National_parks_me_2, Team_sport_me_2, Own_sport_me_2,
+                 Summer_camp_me_2, Hike_outdoors_me_2, Garden_me_2, Experiments_me_2,
+                 Read_me_2, Internet_me_2, Computer_me_2, Communicate_me_2, TV_me_2,
+                 Build_me_2, Pets_me_2,
+                 Library_encourage, OMSI_encourage, Zoo_encourage, Childrens_encourage, Scouts_encourage,
+                 Other_after_encourage, National_park_encourage, Team_sport_encourage, Sport_encourage,
+                 Summer_camps_encourage, Hike_outdoors_encourage, Garden_encourage, Experiment_encourage,
+                 Read_encourage, Internet_encourage, Computer_games_encourage, Communicate_encourage, TV_encourage,
+                 Build_encourage, Pets_encourage,
+                 Stars_planets_2, Mix_materials_2, Weather_2, Human_body_2, Traits_2,
+                 Sudoku_2, Fish_hunt_2, Earthquakes_2, Measure_2, Diseases_2,
+                 Minecraft_2, Planets_space_2, Buildings_bridges_2, Eat_exercise_2, Engines_2,
+                 Rocks_minerals_2, Computers_cell_phones_2, Food_flowers_2, Maps_2, Invent_2,
+                 Community_green_2, Shapes_2, Puzzles_2,
+                 Earth_space_6, Human_bio_6, Tech_eng_6);
 detach(B);
 detach(Encourage);
 
-# Now I want to duplicate some of the records to get back to 174
+# Now I want to duplicate some of the records to get back to 144
 I <- sample.int(nrow(BB), size = nrow(A)-nrow(BB));
 C <- data.frame(BB[I,]);  # Duplicated records
 # Change the survey number so that it stays unique.  I'm going to multiply by 1000;
 C$Survey_number <- C$Survey_number*1000;
 
 # Final data for outputing to csv file for ABM.
-# Take original data B and append resampled records in C.
+# Take original data BB and append resampled records in C.
 D <- rbind(BB,C);
 
 filename <- "initialStudentInput.csv"
